bd6955d9579edef5f0be689f2ebff58438f8b241
Brian <brianleekahhoe15@gmail.com>
2026-01-21 17:27:45 +0800

    Level-0: Rename chatbot to Swaz and add greeting

---
+++ src/main/java/Swaz.java
@@ -0,0 +1,7 @@
+public class Swaz {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Swaz");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
