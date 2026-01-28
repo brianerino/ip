a6098f23aea9046962bf4e0cc689629fcadc27c8
Brian <brianleekahhoe15@gmail.com>
2026-01-28 12:16:59 +0800

    Level-2: Add tasks and list them

--- src/main/java/Swaz.java
+++ src/main/java/Swaz.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class Swaz {
     public static void main(String[] args) {
+        
+        // Chatbot style
         String horizontal = "----------------------------------";
-                
+        
+        // Task List + Count
+        String[] tasks = new String[100];
+        int taskCount = 0;
+        
+        // Opening message
         System.out.println(horizontal);
         System.out.println("Hello! I'm Swaz");
         System.out.println("What can I do for you?");
         System.out.println(horizontal);
 
+        // Get input from user
         Scanner in = new Scanner(System.in);
 
+        // add-to-list, bye, and list logic
         while (true) {
             String input = in.nextLine();
 
@@ -20,8 +29,21 @@ public class Swaz {
                 System.out.println(horizontal);
                 break;
             }
+            
+            if (input.equals("list")) {
+                System.out.println(horizontal);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(horizontal);
+                continue;
+            }
+            
+            tasks[taskCount] = input;
+            taskCount++;
+            
             System.out.println(horizontal);
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println(horizontal);
         }
     }
