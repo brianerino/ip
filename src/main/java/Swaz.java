d012c0a83ae4cfc09afc4d410254a849044caea3
Brian <brianleekahhoe15@gmail.com>
2026-01-28 11:50:16 +0800

    Level-1: Echo input and exit on bye

--- src/main/java/Swaz.java
+++ src/main/java/Swaz.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Swaz {
     public static void main(String[] args) {
+        String horizontal = "----------------------------------";
+                
+        System.out.println(horizontal);
         System.out.println("Hello! I'm Swaz");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal);
+
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            String input = in.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(horizontal);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontal);
+                break;
+            }
+            System.out.println(horizontal);
+            System.out.println(input);
+            System.out.println(horizontal);
+        }
     }
 }
